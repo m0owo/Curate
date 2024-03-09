58ea9de69ff37128819e002218cde209b2dbe371
m0owo <8breadsticks8@gmail.com>
2024-03-09 14:31:11 +0700

    changed search to TextEdit

--- frontend/src/pages/ui/home/home.py
+++ frontend/src/pages/ui/home/home.py
@@ -3,11 +3,18 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from home_ui import Ui_MainWindow
 
+
 class HomeUI(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self, None)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        shadow = QGraphicsDropShadowEffect(self)
+        shadow.setBlurRadius(10)
+        shadow.setXOffset(1)
+        shadow.setYOffset(2)
+
+        self.ui.search_frame.setGraphicsEffect(shadow)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- frontend/src/pages/ui/home/home_ui.py
+++ frontend/src/pages/ui/home/home_ui.py
@@ -15,8 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
-    QPushButton, QSizePolicy, QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QLineEdit,
+    QMainWindow, QPushButton, QSizePolicy, QWidget)
 import icons_rc
 import logo_rc
 
@@ -25,6 +25,7 @@ class Ui_MainWindow(object):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(1080, 720)
+        MainWindow.setMaximumSize(QSize(1080, 720))
         MainWindow.setStyleSheet(u"#centralwidget {\n"
 "    background-color: #FFFFFF;\n"
 "}\n"
@@ -35,7 +36,8 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.header = QWidget(self.centralwidget)
         self.header.setObjectName(u"header")
-        self.header.setGeometry(QRect(20, 10, 1056, 696))
+        self.header.setGeometry(QRect(20, 10, 1080, 720))
+        self.header.setMaximumSize(QSize(1080, 720))
         self.header.setStyleSheet(u"")
         self.frame = QFrame(self.header)
         self.frame.setObjectName(u"frame")
@@ -60,22 +62,32 @@ class Ui_MainWindow(object):
 "QLabel#page_label{\n"
 "    background-color: #E8F3F2;\n"
 "    border-radius: 10px;\n"
+"    border: solid;\n"
+"    border-width: 1px;\n"
+"    border-color:rgb(154, 152, 148)\n"
 "}")
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
         self.search_frame = QFrame(self.frame)
         self.search_frame.setObjectName(u"search_frame")
         self.search_frame.setEnabled(True)
-        self.search_frame.setGeometry(QRect(150, 20, 581, 30))
-        self.search_frame.setFrameShape(QFrame.StyledPanel)
-        self.search_frame.setFrameShadow(QFrame.Raised)
-        self.search_edit = QTextEdit(self.search_frame)
-        self.search_edit.setObjectName(u"search_edit")
-        self.search_edit.setGeometry(QRect(40, 3, 471, 20))
+        self.search_frame.setGeometry(QRect(130, 20, 581, 30))
         font = QFont()
         font.setFamilies([u"Manrope"])
-        self.search_edit.setFont(font)
-        self.search_edit.setStyleSheet(u"")
+        self.search_frame.setFont(font)
+        self.search_frame.setStyleSheet(u"QLineEdit{\n"
+"    color: rgb(190, 123, 193);\n"
+"    background-color: transparent;\n"
+"    border: none;\n"
+"}\n"
+"\n"
+"#search_frame{\n"
+"    border: solid;\n"
+"    border-width: 1px;\n"
+"    border-color: rgb(154, 152, 148);\n"
+"}")
+        self.search_frame.setFrameShape(QFrame.StyledPanel)
+        self.search_frame.setFrameShadow(QFrame.Raised)
         self.search_icon = QLabel(self.search_frame)
         self.search_icon.setObjectName(u"search_icon")
         self.search_icon.setGeometry(QRect(10, 5, 18, 18))
@@ -88,9 +100,13 @@ class Ui_MainWindow(object):
         icon.addFile(u":/icon_images/filter_icon.png", QSize(), QIcon.Normal, QIcon.Off)
         self.filter_button.setIcon(icon)
         self.filter_button.setIconSize(QSize(20, 20))
+        self.search_edit = QLineEdit(self.search_frame)
+        self.search_edit.setObjectName(u"search_edit")
+        self.search_edit.setGeometry(QRect(40, 5, 491, 16))
+        self.search_edit.setFont(font)
         self.home_button = QPushButton(self.frame)
         self.home_button.setObjectName(u"home_button")
-        self.home_button.setGeometry(QRect(760, 20, 41, 30))
+        self.home_button.setGeometry(QRect(740, 20, 41, 30))
         font1 = QFont()
         font1.setFamilies([u"Manrope"])
         font1.setBold(True)
@@ -101,28 +117,29 @@ class Ui_MainWindow(object):
         self.home_button.setIconSize(QSize(20, 20))
         self.history_button = QPushButton(self.frame)
         self.history_button.setObjectName(u"history_button")
-        self.history_button.setGeometry(QRect(800, 20, 41, 30))
+        self.history_button.setGeometry(QRect(780, 20, 41, 30))
         icon2 = QIcon()
         icon2.addFile(u":/icon_images/history_icon.png", QSize(), QIcon.Normal, QIcon.Off)
         self.history_button.setIcon(icon2)
         self.history_button.setIconSize(QSize(20, 20))
         self.wishlist_button = QPushButton(self.frame)
         self.wishlist_button.setObjectName(u"wishlist_button")
-        self.wishlist_button.setGeometry(QRect(840, 20, 41, 30))
+        self.wishlist_button.setGeometry(QRect(820, 20, 41, 30))
         icon3 = QIcon()
         icon3.addFile(u":/icon_images/wishlist_icon.png", QSize(), QIcon.Normal, QIcon.Off)
         self.wishlist_button.setIcon(icon3)
         self.wishlist_button.setIconSize(QSize(20, 20))
         self.profile_button = QPushButton(self.frame)
         self.profile_button.setObjectName(u"profile_button")
-        self.profile_button.setGeometry(QRect(880, 20, 41, 30))
+        self.profile_button.setGeometry(QRect(860, 20, 41, 30))
         icon4 = QIcon()
         icon4.addFile(u":/icon_images/profile_icon.png", QSize(), QIcon.Normal, QIcon.Off)
         self.profile_button.setIcon(icon4)
         self.profile_button.setIconSize(QSize(20, 20))
         self.page_label = QLabel(self.frame)
         self.page_label.setObjectName(u"page_label")
-        self.page_label.setGeometry(QRect(40, 20, 71, 30))
+        self.page_label.setGeometry(QRect(30, 20, 71, 30))
+        self.page_label.setFont(font1)
         self.page_label.setAlignment(Qt.AlignCenter)
         self.frame_2 = QFrame(self.header)
         self.frame_2.setObjectName(u"frame_2")
@@ -131,7 +148,7 @@ class Ui_MainWindow(object):
         self.frame_2.setFrameShadow(QFrame.Raised)
         self.curate_label = QLabel(self.frame_2)
         self.curate_label.setObjectName(u"curate_label")
-        self.curate_label.setGeometry(QRect(-30, -20, 181, 101))
+        self.curate_label.setGeometry(QRect(-10, -10, 161, 91))
         font2 = QFont()
         font2.setFamilies([u"Mogena"])
         font2.setPointSize(18)
@@ -148,16 +165,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.search_edit.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Manrope'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'.AppleSystemUIFont'; color:#d5bbd6;\">Tell us what you're looking for...</span></p></body></html>", None))
         self.search_icon.setText("")
         self.filter_button.setText("")
+        self.search_edit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"tell us what you're looking for...", None))
         self.home_button.setText("")
         self.history_button.setText("")
         self.wishlist_button.setText("")
