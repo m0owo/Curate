14c96666a6732372c2ee59c0622eeb8e83b7d0d1
m0owo <8breadsticks8@gmail.com>
2024-03-30 21:33:09 +0700

    db changes

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import ZODB, ZODB.FileStorage
 import BTrees._OOBTree
 import persistent
 import transaction
-from datetime import datetime
+from datetime import datetime, date
 from PySide6.QtWidgets import (
     QAbstractScrollArea, QApplication, QFrame, QGridLayout,
     QLabel, QLineEdit, QMainWindow, QPushButton,
@@ -11,7 +11,7 @@ from PySide6.QtWidgets import (
 
 #topic tags, link is the placeholder for what the button would do
 class Tag(persistent.Persistent):
-    def __init__(self, tag_text, link):
+    def __init__(self, tag_text, link = None):
         self.tag_text = tag_text
         self.link = link #what does the tag do
     def get_tag_text(self):
@@ -22,13 +22,13 @@ class Tag(persistent.Persistent):
 #product in the post
 class Product(persistent.Persistent):
     def __init__(self, pr_name, pr_id, seller, status, start, tags, end = None):
+    def __init__(self, pr_name, pr_id, seller, status, start, end = None):
         self.pr_name = pr_name
         self.pr_id = pr_id
         self.seller = seller
         self.status = status #upcoming, live, sold
         self.start_date = start #time to go live
         self.end_date = end #Date object for time sold
-        self.tags = tags #list of Tag objects
     def get_name(self):
         return self.pr_name
     def get_title(self):
@@ -37,8 +37,6 @@ class Product(persistent.Persistent):
         return self.seller
     def get_status(self):
         return self.status
-    def get_tags(self):
-        return self.tags
     def get_start_date(self):
         return self.start_date
     def get_end_date(self):
@@ -49,34 +47,48 @@ class Product(persistent.Persistent):
     
 #collection of items in a post, items would be persistent list in real??
 class Collection(Product):
-    def __init__(self, c_name, c_id, seller, status, start, end, tags, items = {}):
-        super.__init__(c_name, c_id, seller, status, start, end, tags)
-        self.items = items #Item objects in the collection
+    def __init__(self, c_name, c_id, seller, status, start, items, end = None,):
+        super.__init__(c_name, c_id, seller, status, start, end)
+        self.items = persistent.PersistentList() #Item objects in the collection
+        self.tags = persistent.PersistentList()
+        for i in items:
+            self.items.append(i)
+            for t in items.get_tags():
+                self.tags.append(t)
     def get_items(self):
         return self.items
 
 #individual items in a post
 class Item(Product):
-    def __init__(self, i_name, i_id, seller, status, start, end, tags, price, image):
-        super.__init__(i_name, i_id, seller, status, start, end, tags)
+    def __init__(self, i_name, i_id, seller, status, start, tags, price, image, end = None):
+        super.__init__(i_name, i_id, seller, status, start, end)
         self.price = price
         self.image = image
+        self.tags = persistent.PersistentList()
+        for t in tags:
+            self.tags.append(t)
     def get_price(self):
         return self.price
     def get_image(self):
         return self.image #how to store images in a class
+    def get_tags(self):
+        return self.tags
 
 #info to display in post
 class PostDetails(persistent.Persistent):
     def __init__(self, p_id, p_author, p_info, p_title,
-                 p_tags, type = "CF no CC",
-                 product = []):
+                 product, type = "CF no CC"):
         self.id = p_id
         self.author = p_author
-        self.product = product #a single item is a collection size 1
+        self.product = persistent.PersistentList() #a single item is a collection size 1
+        self.tags = persistent.PersistentList()
+        for i in product:
+            self.product.append(i)
+            for t in i.get_tags:
+                if t not in self.tags:
+                    self.tag.append(t)
         self.info = p_info
         self.title = p_title
-        self.tags = p_tags
         self.created = datetime.now()
         self.modified = self.created
         if len(product) > 1:
@@ -99,8 +111,6 @@ class PostDetails(persistent.Persistent):
     def get_product_type(self):
         return self.pr_type
     def get_tags(self):
-        # for p in product:
-        #     self.tags.append(p.get_tags())
         return self.tags
     def get_sales_type(self):
         return self.sales_type
@@ -110,10 +120,8 @@ class Account(persistent.Persistent):
         self.gmail = gmail
         self.password = password
         self.username = ""
-
     def get_email(self):
         return self.gmail
-    
     def get_password(self):
         return self.password
 
@@ -128,6 +136,17 @@ root = connection.root
 root.admins = BTrees.OOBTree.BTree()
 root.admins[1000] = Admin("admin1@gmail.com", "1234")
 
+root.tags = BTrees.OOBTree.BTree()
+root.tags[1000] = Tag("secondhand")
+root.tags[1001] = Tag("fashion")
+
+root.products = BTrees.OOBTree.BTree()
+root.product[1000] = Collection("Mona Tops Collection",
+                                1000, "Mona", "Upcoming",
+                                date(2024, 5, 16), ,
+                                []
+)
+
 transaction.commit()
 
 if  __name__ == "__main__":
@@ -135,8 +154,6 @@ if  __name__ == "__main__":
     for a in admins:
         admin = admins[a]
         print(admin.get_email())
-
-        
     import sys
     print(sys.path)
 
