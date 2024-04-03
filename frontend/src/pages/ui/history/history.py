4206a9fc67deb6228e738d0b784f3f94f2e75414
Sirapop Tuntithanakij <sirapop33408@gmail.com>
2024-04-03 17:35:37 +0700

    product, wishlist, collection, history ui

--- frontend/src/pages/ui/history/history.py
+++ frontend/src/pages/ui/history/history.py
@@ -10,6 +10,18 @@ class HistoryUI(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.ui.home_button.clicked.connect(self.to_home_page)
+        self.ui.wishlist_button.clicked.connect(self.to_wishlist_page)
+        self.ui.profile_button.clicked.connect(self.to_profile_page)
+
+    def to_home_page(self):
+        self.stacked_widget.setCurrentIndex(1)
+    def to_profile_page(self):
+        self.stacked_widget.setCurrentIndex(3)
+    def to_wishlist_page(self):
+        self.stacked_widget.setCurrentIndex(6)
+
+    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     history_ui = HistoryUI()
--- frontend/src/pages/ui/wishlist/wishlist.py
+++ frontend/src/pages/ui/wishlist/wishlist.py
@@ -10,6 +10,17 @@ class WishlistUI(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.ui.home_button.clicked.connect(self.to_home_page)
+        self.ui.history_button.clicked.connect(self.to_history_page)
+        self.ui.profile_button.clicked.connect(self.to_profile_page)
+
+    def to_home_page(self):
+        self.stacked_widget.setCurrentIndex(1)
+    def to_profile_page(self):
+        self.stacked_widget.setCurrentIndex(3)
+    def to_history_page(self):
+        self.stacked_widget.setCurrentIndex(5)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     wishlist_ui = WishlistUI()
